f949890dc2893c90c256667fb8f85a9a632aa5e8
rozhanalvandi <129389370+rozhanalvandi@users.noreply.github.com>
2024-01-25 20:59:13 +0330

    add hash class

--- treeFamily/treeFamily.py
+++ treeFamily/treeFamily.py
@@ -1,6 +1,6 @@
-
-from hash import sha256
+from Hash import sha256
 import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
 import networkx as nx
 
 class TreeNode:
@@ -95,3 +95,200 @@ class Tree:
         print(f"{indent}{node.name}")
         for child in node.children:
             self._display_tree(child, level + 1)
+    def are_parent_and_child(self, name1, name2):   #balanced O(log N) , else O(N)
+        hash_name1 = name1
+        hash_name2 = name2
+
+        node1 = self.find_node(hash_name1)
+        node2 = self.find_node(hash_name2)
+
+        if node1 and node2:
+            return node2 in node1.children or node1 in node2.children
+        return False
+
+    def check_siblings(self, name1, name2):       #balanced O(log N) , else O(N)
+        hash_name1 = name1
+        hash_name2 = name2
+
+        node1 = self.find_node(hash_name1)
+        node2 = self.find_node(hash_name2)
+
+        if node1 and node2:
+            return any(child in node2.parents[0].children for child in node1.parents[0].children)
+        return False
+
+    def are_family_related(self, name1, name2):     #balanced O(log N) , else O(N)
+        hash_name1 = name1
+        hash_name2 = name2
+
+        node1 = self.find_node(hash_name1)
+        node2 = self.find_node(hash_name2)
+
+        if node1 and node2:
+            if node2 in node1.children or node1 in node2.children:
+                return False
+
+            if node2.parents and node1 in node2.parents[0].children:
+                return False
+            elif node1.parents and node2 in node1.parents[0].children:
+                return False
+
+            return True
+        else:
+            return False
+
+    def find_child_count(self, node_name):     #O(1)
+        node = self.find_node(node_name)
+
+        if node is None:
+            return 0
+
+        return len(node.children)
+
+    def find_farthest_relationship(self, people):  #O(n^2)
+
+        max_distance = 0
+        farthest_pair = (None, None)
+        for person1 in people:
+            for person2 in people:
+                if person1 != person2:
+                    distance = self.calculate_distance(person1, person2, people)
+                    if distance == "no relationship":
+                        continue
+                    if distance > max_distance:
+                        max_distance = distance
+                        farthest_pair = (person1, person2)
+        return farthest_pair
+
+    def calculate_distance(self, name1, name2, people):  #O(n)
+
+        ancestor = self.lca(name1, name2)
+        if ancestor is None:
+            return "no relationship"
+        distance1 = 0
+        distance2 = 0
+        current1 = name1
+        current2 = name2
+        while current1 and current1 != ancestor:
+            current1 = people.get(current1, None)
+            if current1:
+             current1 = current1.father
+             distance1 += 1
+
+        while current2 and current2 != ancestor:
+         current2 = people.get(current2, None)
+         if current2:
+          current2 = current2.father
+          distance2 += 1
+
+        return distance2 + distance1
+
+class TreeVisualizer:
+    def __init__(self, family_tree):
+        self.family_tree = family_tree
+        self.graph = nx.DiGraph()
+
+    def build_graph(self, node, parent=None):
+        self.graph.add_node(node.name)
+        if parent:
+            self.graph.add_edge(parent.name, node.name)
+        for child in node.children:
+            self.build_graph(child, parent=node)
+
+    def visualize_tree(self):
+        self.build_graph(self.family_tree.root)
+        pos = self._layout_tree(self.family_tree.root)
+        nx.draw(self.graph, pos, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue', font_color='black')
+        plt.show()
+
+    def _layout_tree(self, current_node, pos=None, x=0, level=1, width=2.0, vert_gap=0.4, xcenter=0.5):
+        if pos is None:
+            pos = {current_node.name: (x, -level * vert_gap)}
+        else:
+            pos[current_node.name] = (x, -level * vert_gap)
+        neighbors = list(self.graph.neighbors(current_node.name))
+        if len(neighbors) != 0:
+            dx = width / 2
+            nextx = x - width / 2 - dx / 2
+            for neighbor_name in neighbors:
+                neighbor_node = self.family_tree._find_node(neighbor_name, current_node)  # Pass the current_node
+                if isinstance(neighbor_node, TreeNode):  # Check if it's a TreeNode
+                    nextx += dx
+                    pos = self._layout_tree(neighbor_node, pos=pos, x=nextx, level=level-1, width=dx, vert_gap=vert_gap, xcenter=xcenter)
+        return pos
+
+# Define people dictionary
+class Person:
+    def __init__(self, father):
+        self.father = father
+
+people = {
+    'Grandpa': Person(None),
+    'Grandma': Person(None),
+    'Dad': Person('Grandpa'),
+    'Mom': Person(None),
+    'Son': Person('Dad'),
+    'Daughter': Person('Dad'),
+    'Uncle': Person('Grandpa'),
+    'Aunt': Person('Grandma'),
+    'Cousin': Person('Uncle'),
+    'Nephew': Person('Son'),
+    'Niece': Person('Daughter')
+}
+
+grandpa = TreeNode("Grandpa")
+grandma = TreeNode("Grandma")
+dad = TreeNode("Dad", parents=[grandpa, grandma])
+mom = TreeNode("Mom")
+son = TreeNode("Son", parents=[dad, mom])
+daughter = TreeNode("Daughter", parents=[dad, mom])
+uncle = TreeNode("Uncle", parents=[grandpa, grandma])
+aunt = TreeNode("Aunt", parents=[grandpa, grandma])
+cousin = TreeNode("Cousin", parents=[uncle, aunt])
+nephew = TreeNode("Nephew", parents=[son])
+niece = TreeNode("Niece", parents=[daughter])
+
+grandpa.add_child(dad)
+grandma.add_child(dad)
+dad.add_child(son)
+dad.add_child(daughter)
+grandpa.add_child(uncle)
+grandma.add_child(uncle)
+grandpa.add_child(aunt)
+grandma.add_child(aunt)
+uncle.add_child(cousin)
+son.add_child(nephew)
+daughter.add_child(niece)
+
+print("Family Tree:")
+family_tree = Tree(root=grandpa)
+family_tree.display_tree()
+
+# Check are_parent_and_child?
+parent_result = family_tree.are_parent_and_child('Daughter', 'Son')
+print(f"Are parent and child? {parent_result}")
+
+# Check are siblings or not?
+siblings_result = family_tree.check_siblings('Son', 'Daughter')
+print(f"Are siblings? {siblings_result}")
+
+# Check family relationship distance
+family_related = family_tree.are_family_related('Son', 'Daughter')
+print(f"Are they family? {family_related}")
+
+# Find Least Common Ancestor
+common_result = family_tree.lca('Cousin', 'Son')
+print(f"The common Ancestor is {common_result}")
+
+# Get descendant count
+descendant_count = family_tree.find_child_count('Grandpa')
+print(f"The descendant count is {descendant_count}")
+
+# Find furthest nodes in the relationship
+furthest_node1, furthest_node2 = family_tree.find_farthest_relationship(people)
+print(f"The furthest nodes are: {furthest_node1}, {furthest_node2}")
+
+
+# Assuming family_tree is already created
+visualizer = TreeVisualizer(family_tree)
+visualizer.visualize_tree()
